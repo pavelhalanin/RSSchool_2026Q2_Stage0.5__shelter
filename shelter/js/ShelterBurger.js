79da4069b8e32d1b9c5f4e50bda15940c1a99c37
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-06-29 02:56:24 +0300

    feat: no scroll added on opened menu

--- shelter/js/ShelterBurger.js
+++ shelter/js/ShelterBurger.js
@@ -1,21 +1,13 @@
 class ShelterBurger {
-  static id_header = "header";
-
   static openOrClose() {
-    const HEADER = document.getElementById(this.id_header);
-
-    if (!HEADER) {
-      console.error(`Узел не найден: ${this.id_header}`);
-      return;
-    }
-
-    const IS_OPENED = HEADER.getAttribute("data-is-menu-opened") == "true";
+    const IS_OPENED =
+      document.body.getAttribute("data-is-menu-opened") == "true";
 
     if (IS_OPENED) {
-      HEADER.removeAttribute("data-is-menu-opened");
+      document.body.removeAttribute("data-is-menu-opened");
       return;
     }
 
-    HEADER.setAttribute("data-is-menu-opened", "true");
+    document.body.setAttribute("data-is-menu-opened", "true");
   }
 }
